e01ea23fd4e8ed6b8975a19223a57d0071fe303d
Sanket <19sanket95@gmail.com>
2018-10-29 08:08:29 +0530

    Setup a new angular 7 project and created 4 new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+/**
+ * Component Decorator
+ */
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app-root', //uniqur name
+  templateUrl: './app.component.html', //specifies the html for the component
   styleUrls: ['./app.component.scss']
 })
+
+/**
+ * Logic of the component
+ */
 export class AppComponent {
   title = 'ng7Pilot';
 }
